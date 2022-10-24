7508b2af24252534c52a0a9ef82622ccac99c928
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2022-10-25 00:20:05 +0300

    settings.py changes discarded, models.py modified

--- school/settings.py
+++ school/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- school_app/models.py
+++ school_app/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 
+from datetime import datetime
+
 # Create your models here.
 
 
 class Push(models.Model):
-    pass
+    content = models.CharField("Содержание", max_length=256)
+    type = models.IntegerField("Источник уведомления", default=1)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name = "Уведомление"
+        verbose_name_plural = "Уведомления"
 
 
 class File(models.Model):
@@ -38,17 +48,24 @@ class Lesson(models.Model):
 
 class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
-    name = models.CharField(default="Артём)")
-    surname = models.CharField(default="Самотохин)")
-    email = models.EmailField("Почта")
-    registered = models.DateTimeField()
-    notifications = models.ManyToManyField(Push)
-    available_courses = models.CharField()
+    name = models.CharField("Имя", max_length=32, default="Артём)")
+    surname = models.CharField("Фамилия", max_length=32, default="Самотохин)")
+    email = models.EmailField("Почта", max_length=32)
+    registered = models.DateTimeField("Зарегистрировался", default=datetime.now())
+    notifications = models.ManyToManyField(Push, "Уведомления")
+    available_courses = models.CharField("Доступные курсы", max_length=2048)
 
     # progresses = models.ManyToManyField(Progress)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
 
 class Teacher(User):
