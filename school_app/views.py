c1fe867090a033416760074f0fab50c55c7f4cd9
fluxx1on <nick.kaliga@ya.ru>
2022-10-26 00:28:15 +0300

    .05
    - Added templates
    - Added static

--- school_app/views.py
+++ school_app/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'pivo': 'tozepivo'}
+    if request.method == 'GET':
+        print('hello Percy')
+        print(request.GET)
+    elif request.method == 'POST':
+        print(request.POST.get('name'))
+        print(1)
+    return render(request, 'index.html', context)
