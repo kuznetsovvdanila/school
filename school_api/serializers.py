6755319acbb0ec1ff865e1f259415c619c9af603
fluxx1on <nick.kaliga@ya.ru>
2022-11-15 16:40:16 +0300

    update Serializers

--- school_api/serializers.py
+++ school_api/serializers.py
@@ -10,12 +10,20 @@ class LessonSerializer(serializers.ModelSerializer):
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
+        exclude = ['password', 'email']
 
 class CourseSerializer(serializers.ModelSerializer):
-    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
         fields = ['id', 'name', 'product_preview', 'value', 'lessonsCount', 
-        'duration', 'date_open', 'lessons', ]
+        'duration', 'date_open']
+
+class CourseSerializerM2m(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+    teachers = TeacherSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['lessons', 'teachers']
         
--- school_api/views.py
+++ school_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework_api_key.models import APIKey
 from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.serializers import ListSerializer
 
 from school_app.models import *
 from .serializers import *
@@ -30,13 +31,19 @@ from .serializers import *
 
 @api_view(("GET", "POST"))
 def getCourse(request):
+    context = list()
     try:
         key = request.META["HTTP_AUTHORIZATION"].split()[0]
         getApi = APIKey.objects.get_from_key(key)
         if getApi is not None:
             queryset = Course.objects.all().order_by('name')
             serializer = CourseSerializer(instance=queryset, many=True)
-            return Response(serializer.data)
+            serializerm2m = CourseSerializerM2m(instance=queryset, many=True)
+            for i in range(len(serializer.data)):
+                context.append(dict())
+                context[i].update(serializer.data[i])
+                context[i].update(serializerm2m.data[i])
+            return Response(context)
         else: return Response(status_code=404)
     except KeyError:
         return Response(status=500)
